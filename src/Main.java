0e9f7ce7572f90a9c53921bf82eb6c6af01a62b7
ZakharBesagin <zakhar.besagin@gmail.com>
2023-04-26 23:22:37 +0200

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -15,8 +18,35 @@ public class Main {
         }
         System.out.println(sum);
 
+        String myString = "Hello World!!!";
+
+        char[] result = myString.toCharArray();
+
+        Character maxOccurSymb = null;
+
+        int maxOccurNumb = 0;
+
+        for (char word : result) {
+            Map<Character, Integer> helpMap = new HashMap<>();
+            for (Character character : result) {
+                if (helpMap.containsKey(character)) {
+                    Integer value = helpMap.get(character) + 1;
+                    helpMap.put(character, value);
+                } else {
+                    helpMap.put(character, 1);
+                }
+            }
+
+            for (Character char123 : helpMap.keySet()) {
+                if (helpMap.get(char123) > maxOccurNumb) {
+                    maxOccurNumb = helpMap.get(char123);
+                    maxOccurSymb = char123;
+                }
+            }
+        }
+        System.out.println("maxOccurxSymb: " + maxOccurSymb + ";  maxOccur = " + maxOccurNumb);
     }
 
+    }
 
-}
 
